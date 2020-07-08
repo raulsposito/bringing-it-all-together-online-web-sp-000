07982c22097c18d42a489492a9dad1a9aa7fe027
Raul Sposito <raulsposito@gmail.com>
2020-07-08 05:01:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,5 +8,13 @@ class Dog
     @breed = breed
   end
 
-
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL 
+    DB[:conn].execute(sql)
 end
