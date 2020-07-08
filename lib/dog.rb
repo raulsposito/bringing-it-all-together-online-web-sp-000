de2e8e1e32fe4744d1fc7fdb6174b2262a2ef075
Raul Sposito <raulsposito@gmail.com>
2020-07-08 05:06:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,4 +23,20 @@ class Dog
     sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
   end
+
+  def save 
+    if self.id 
+      self.update
+    else 
+      sql = <<-SQL 
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL 
+      DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      end
+      self
+    end
+      
+  end
+  
 end
